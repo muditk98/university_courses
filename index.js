1088672297b340012c151d7ea7d43df0fc66c4b6
TheWussPuss <kapsmudit@gmail.com>
2019-02-25 22:25:08 +0530

    initial template

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+let app = express();
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+app.get('/course', (req, res) => {
+    res.render('course', {
+        course_name: "JS"
+    });
+});
+
+app.post('/course', (req, res) => {
+    res.render('course', {
+        course_name: req.body.course_name
+    })
+})
+
+app.listen(3000);
