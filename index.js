d5f5d3180b45d7bd78892c20ed5216b399e00bd9
TheWussPuss <kapsmudit@gmail.com>
2019-03-03 01:22:03 +0530

    temp

--- index.js
+++ index.js
@@ -2,23 +2,31 @@ const express = require('express');
 
 let app = express();
 app.use(express.urlencoded({
-    extended: true
+	extended: true
 }));
 app.use(express.json());
+
+
 
 app.set('views', './views');
 app.set('view engine', 'pug');
 
 app.get('/course', (req, res) => {
-    res.render('course', {
-        course_name: "JS"
-    });
-});
+	res.render('course', {
+		course_name: "JS"
+	})
+})
 
 app.post('/course', (req, res) => {
-    res.render('course', {
-        course_name: req.body.course_name
-    })
+	if (req.body.action == 'add' && courseExists(id)) {
+		
+	}
+})
+
+app.post('/course', (req, res) => {
+	res.render('course', {
+		course_name: req.body.course_name
+	})
 })
 
 app.listen(3000);
